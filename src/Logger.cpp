e0d81c5fea502eca4d86f11bcd23da307ececb07
Carsten Pohl <c@rstenpohl.de>
2014-09-05 22:41:02 +0200

    more logger source

---
+++ src/Logger.cpp
@@ -0,0 +1,15 @@
+#include "Logger.hpp"
+
+#include <iostream>
+
+void Logger::error() {
+        std::cout << "[ERROR  ]" << std::endl;
+}
+
+void Logger::warn() {
+        std::cout << "[ERROR  ]" << std::endl;
+}
+
+void Logger::debug() {
+        std::cout << "[ERROR  ]" << std::endl;
+}
---
+++ src/Logger.hpp
@@ -0,0 +1,16 @@
+#ifndef LOGGER_HPP
+#define LOGGER_HPP
+
+#include <string>
+#include <vector>
+
+class Logger {
+        private:
+        protected:
+        public:
+                void error();
+                void warn();
+                void debug();
+};
+
+#endif
